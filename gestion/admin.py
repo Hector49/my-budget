c36fcb0daa6a82e92b5a2cab794584a9724d36b8
hector49 <hector49@hotmail.fr>
2017-10-02 14:51:08 +0200

    My app budget, first commit

---
+++ gestion/admin.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from gestion.models import Ecriture, Compte, Mouvement, Poste, Cause
+# Register your models here.
+
+
+class EcritureAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
+    list_display   = ('compte', 'jour', 'date', 'mouv',
+    'poste', 'cause', 'debit', 'credit', 'commentaires')
+    list_filter    = ('poste','cause','compte', 'mouv')
+    ordering_reverse       = ('date', )
+    
+    fields = ('compte', 'jour', 'date', 'mouv', 'poste', 'cause', 'debit', 'credit', 'commentaires', 'validee', 'mensuelle')
+
+class CompteAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
+    list_display   = ('numero', 'libelle', 'solde')
+    list_filter    = ('numero','libelle', )
+    ordering       = ('id', )
+    
+    fields = ('numero', 'libelle','solde')
+
+class MouvementAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
+    list_display   = ('mouv', 'libelle', 'compte')
+    list_filter    = ('mouv','libelle', )
+    ordering       = ('id', )
+    
+    fields = ('mouv', 'libelle','compte')
+
+class PosteAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
+    list_display   = ('nom', 'cumul', 'compte')
+    list_filter    = ('nom','compte', )
+    ordering       = ('id', )
+    
+    fields = ('nom', 'cumul','compte')
+
+class CauseAdmin(admin.ModelAdmin):
+
+    # Configuration de la liste d'articles
+    list_display   = ('nom', 'cumul', 'compte')
+    list_filter    = ('nom','compte', )
+    ordering       = ('id', )
+    
+    fields = ('nom', 'cumul','compte')
+
+admin.site.register(Ecriture, EcritureAdmin)
+admin.site.register(Compte, CompteAdmin)
+admin.site.register(Poste, PosteAdmin)
+admin.site.register(Cause, CauseAdmin)
+admin.site.register(Mouvement, MouvementAdmin)
---
+++ gestion/models.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+# Create your models here.
+
+class Ecriture(models.Model):
+    compte = models.ForeignKey('Compte')
+    jour = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, 
+                                verbose_name="Date d'entrée")
+    mouv = models.ForeignKey('Mouvement')
+    poste = models.ForeignKey('Poste')
+    cause = models.ForeignKey('Cause')
+    debit = models.DecimalField(max_digits=10,decimal_places=2)
+    credit = models.DecimalField(max_digits=10,decimal_places=2)
+    commentaires = models.TextField(null=True)
+    validee = models.CharField(max_length=1)
+    mensuelle = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.commentaires
+    
+class Compte(models.Model):
+    numero = models.CharField(max_length=25)
+    libelle = models.TextField(null=False)
+    solde = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.libelle
+
+class Cause(models.Model):
+    nom = models.CharField(max_length=50)
+    cumul = models.DecimalField(max_digits=10,decimal_places=2)
+    compte = models.ForeignKey('Compte')
+
+    def __str__(self):
+        return self.nom
+
+class Poste(models.Model):
+    nom = models.CharField(max_length=50)
+    cumul = models.DecimalField(max_digits=10,decimal_places=2)
+    compte = models.ForeignKey('Compte')
+
+    def __str__(self):
+        return self.nom
+
+class Mouvement(models.Model):
+    mouv = models.CharField(max_length=2)
+    libelle = models.CharField(max_length=25)
+    cumul = models.DecimalField(max_digits=10,decimal_places=2)
+    compte = models.ForeignKey('Compte')
+
+    def __str__(self):
+        return self.mouv
