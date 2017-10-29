c08f49431f2ce2b80fb42c134ba93c07b7427b0d
hector49 <hector49@hotmail.fr>
2017-10-29 14:06:13 +0100

    ajout de la vasualisation pour des vues

--- banque/urls.py
+++ banque/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^prive/', admin.site.urls),
     url(r'^gestion/', include('gestion.urls')),
 
 ]
